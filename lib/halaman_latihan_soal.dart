4581bda73e0427ba9fbc594f8d462c8365b82ef6
sindypratiwi <luhgedesindypratiwi12@gmail.com>
2025-06-09 22:54:11 +0800

    menambahkan kata-kata mutiara sebelum slide latihan soal dan menambahkan gambar pada setiap soal

--- lib/halaman_latihan_soal.dart
+++ lib/halaman_latihan_soal.dart
@@ -1,7 +1,6 @@
-
+import 'package:aplikasi_belajar_bahasa/custom_bottom_navbar.dart';
+import 'package:aplikasi_belajar_bahasa/halamanutama.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bahasa_inggris_1/custom_button_navbar.dart';
-import 'package:flutter_bahasa_inggris_1/halaman_utama.dart';
 
 class HalamanLatihanSoal extends StatefulWidget {
   const HalamanLatihanSoal({super.key});
@@ -11,31 +10,76 @@ class HalamanLatihanSoal extends StatefulWidget {
 }
 
 class _HalamanLatihanSoalState extends State<HalamanLatihanSoal> {
-  int currentQuestionIndex = 0;
+  int currentQuestionIndex = -1;
   int? selectedOptionIndex;
   bool answerChecked = false;
 
   final List<Map<String, dynamic>> questions = [
     {
-      'question': 'Apa arti dari kata “Banana”?',
-      'options': ['Gajah', 'Pisang', 'Anggur', 'Meja'],
+      'question': 'What is this?',
+      'options': ['Elephant', 'Banana', 'Grape', 'Yellow'],
       'answerIndex': 1,
+      'image': 'assets/images/banana.png',
     },
-    {
-      'question': 'Apa arti dari kata “Table”?',
-      'options': ['Kursi', 'Lemari', 'Meja', 'Pintu'],
-      'answerIndex': 2,
+     {
+      'question': 'What is this?',
+      'options': ['Nose', 'Mouth', 'Eyebrow', 'Goat'],
+      'answerIndex': 1,
+      'image': 'assets/images/mouth.png',
     },
-    {
-      'question': 'Apa arti dari kata “Cat”?',
-      'options': ['Kucing', 'Anjing', 'Ayam', 'Kambing'],
+     {
+      'question': 'What is this?',
+      'options': ['Lung', 'Mouth', 'Eyebrow', 'Soursop'],
       'answerIndex': 0,
+      'image': 'assets/images/lung.png',
     },
+     {
+      'question': 'What is this?',
+      'options': ['Nose', 'Mouth', 'Eyebrow', 'Goat'],
+      'answerIndex': 3,
+      'image': 'assets/images/goat.png',
+    },
+     {
+      'question': 'What is this?',
+      'options': ['Lotus', 'Mouse', 'Grape', 'Lily'],
+      'answerIndex': 0,
+      'image': 'assets/images/lotus.png',
+    },
+     {
+      'question': 'What is this?',
+      'options': ['Kidney', 'Mouth', 'Lung', 'Nose'],
+      'answerIndex': 0,
+      'image': 'assets/images/kidney.png',
+    },
+     {
+      'question': 'What is this?',
+      'options': ['Door', 'Goat', 'Mouse', 'Dog'],
+      'answerIndex': 3,
+      'image': 'assets/images/dog.png',
+    },
+     {
+      'question': 'What is this?',
+      'options': ['Chair', 'Mouth', 'Dog', 'Goat'],
+      'answerIndex': 0,
+      'image': 'assets/images/chair.png',
+    },
+     {
+      'question': 'What is this?',
+      'options': ['Heart', 'Mouth', 'Kidney', 'Goat'],
+      'answerIndex': 0,
+      'image': 'assets/images/heart.png',
+    },
+      {
+      'question': 'What is this?',
+      'options': ['Tiger', 'Goat', 'Mouse', 'Cow'],
+      'answerIndex': 3,
+      'image': 'assets/images/cow.png',
+     } 
+
   ];
 
   void handleOptionTap(int index) {
     if (answerChecked) return;
-
     setState(() {
       selectedOptionIndex = index;
       answerChecked = true;
@@ -73,8 +117,6 @@ class _HalamanLatihanSoalState extends State<HalamanLatihanSoal> {
 
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[currentQuestionIndex];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -86,51 +128,96 @@ class _HalamanLatihanSoalState extends State<HalamanLatihanSoal> {
           ),
         ),
         centerTitle: true,
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.blue[800],
       ),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              currentQuestion['question'],
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            const SizedBox(height: 16),
-            ...List.generate(
-              currentQuestion['options'].length,
-              (index) => Padding(
-                padding: const EdgeInsets.symmetric(vertical: 6),
-                child: _buildOption(
-                  index,
-                  currentQuestion['options'][index],
-                  currentQuestion['answerIndex'],
-                ),
-              ),
-            ),
-            const Spacer(),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: ElevatedButton(
-                onPressed: handleNext,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                ),
-                child: const Text(
-                  'Next',
-                  style: TextStyle(fontSize: 16, color: Colors.white),
-                ),
-              ),
-            ),
-          ],
-        ),
+        child: currentQuestionIndex == -1
+            ? _buildIntroSlide()
+            : _buildQuestionContent(questions[currentQuestionIndex]),
       ),
       bottomNavigationBar: const CustomBottomNavBar(currentIndex: 2),
+    );
+  }
+
+  Widget _buildIntroSlide() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Belajar bahasa, membuka jendela dunia',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 32),
+          ElevatedButton(
+            onPressed: handleNext,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue[800],
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            ),
+            child: const Text(
+              'Mulai',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQuestionContent(Map<String, dynamic> question) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (question['image'] != null) ...[
+          Center(
+            child: Image.asset(
+              question['image'],
+              height: 150,
+            ),
+          ),
+          const SizedBox(height: 20),
+        ],
+        Text(
+          question['question'],
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 16),
+        ...List.generate(
+          question['options'].length,
+          (index) => Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6),
+            child: _buildOption(
+              index,
+              question['options'][index],
+              question['answerIndex'],
+            ),
+          ),
+        ),
+        const Spacer(),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: ElevatedButton(
+            onPressed: handleNext,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue[800],
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            ),
+            child: const Text(
+              'Next',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -158,7 +245,7 @@ class _HalamanLatihanSoalState extends State<HalamanLatihanSoal> {
       child: Align(
         alignment: Alignment.centerLeft,
         child: Text(
-          text,
+          '${String.fromCharCode(65 + index)}. $text', // Menampilkan A. B. C. D.
           style: TextStyle(
             color: buttonColor != null ? Colors.white : Colors.black,
           ),
