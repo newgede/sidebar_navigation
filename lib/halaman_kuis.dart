1e4f4d8d1f9f7ce7f118322530b7b0693e774060
newgede <gedekhrisnapertama18@gmail.com>
2025-06-19 22:46:08 +0800

    perbaikan

--- lib/halaman_kuis.dart
+++ lib/halaman_kuis.dart
@@ -435,30 +435,27 @@ class _HalamanKuisState extends State<HalamanKuis> {
   }
 
   // --- _buildCategorySelectionScreen() ---
-  Widget _buildCategorySelectionScreen() {
-    return SingleChildScrollView( // Membungkus dengan SingleChildScrollView
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.8),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.95),
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      blurRadius: 10,
-                      offset: const Offset(0, 4),
-                    ),
-                  ],
-                ),
+   Widget _buildCategorySelectionScreen() {
+    return Container(
+      color: Colors.white, // Mengubah background menjadi putih penuh
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white, // Mengubah menjadi putih penuh
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 10,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
+              ),
                 child: const Column(
                   children: [
                     Icon(
@@ -512,7 +509,6 @@ class _HalamanKuisState extends State<HalamanKuis> {
             ],
           ),
         ),
-      ),
     );
   }
 
